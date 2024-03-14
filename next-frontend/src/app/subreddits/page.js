d25040ce2b70b1abef3e50ac49a0516a9e48c9f0
Larissa Troper <lmtroper@uwaterloo.ca>
2024-03-13 23:52:26 -0400

    rough draft for login page

---
+++ next-frontend/src/app/subreddits/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SubredditPage = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default SubredditPage
---
+++ next-frontend/src/components/AppDescription.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const AppDescription = () => {
+  return (
+    <div className='h-screen flex flex-col justify-center pl-40'>
+        <div className='text-3xl mb-5'>
+            Reddit Newsletter
+        </div>
+        <div className='text-5xl mb-8'>
+            A one-stop tool for Reddit users
+        </div>
+        <div className='text-lg'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
+    </div>
+  )
+}
+
+export default AppDescription
