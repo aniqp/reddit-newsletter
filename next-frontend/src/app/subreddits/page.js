66fb68a2d5b2a3af49e873d4325deee68bbf7ebd
Larissa Troper <lmtroper@uwaterloo.ca>
2024-05-01 14:58:03 -0400

    subreddits main page

--- next-frontend/src/app/subreddits/page.js
+++ next-frontend/src/app/subreddits/page.js
@@ -1,16 +1,14 @@
 import UserSubreddits from '@/components/UserSubreddits'
 import AccountSettings from '@/components/AccountSettings';
-import React from 'react';
-
 
 export default function SubredditPage({ params }) { 
   return (
-    <div className='flex'>
-        <div className='w-full h-screen flex flex-col xl:flex-row lg:flex-row md:flex-row pl-20 pt-10'>
-          <div className='w-1/3'>
+    <div className='flex-col'>
+        <div className='w-full h-screen flex flex-col lg:flex-row lg:pl-20 lg:pr-20 pl-7 pr-7 pt-10'>
+          <div className='lg:w-1/3'>
             <AccountSettings />
           </div>
-          <div className='w-2/3'>
+          <div className='mt-16 lg:mt-0 lg:w-2/3'>
             <UserSubreddits userId={params.id} />
           </div>
         </div>
